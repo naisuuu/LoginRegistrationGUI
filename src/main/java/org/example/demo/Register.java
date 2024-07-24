fd08dc7fba6586807ecdf679d9d840c4b6a74393
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-07-24 03:47:59 +0100

    created register class, created user class

---
+++ src/main/java/org/example/demo/Register.java
@@ -0,0 +1,9 @@
+package org.example.demo;
+
+public class Register {
+    public void userRegister(String username, String password) {
+        var url = "jdbc:sqlite:users.db";
+
+        var default_users = new String
+    }
+}
