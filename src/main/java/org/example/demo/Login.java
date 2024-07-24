8b9c0eb5a43113e48ebed780c21dc0c6ac6c992a
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-07-24 18:02:10 +0100

    added user/admin pages, updated userlogin to accommodate different logins

--- src/main/java/org/example/demo/Login.java
+++ src/main/java/org/example/demo/Login.java
@@ -2,10 +2,16 @@ package org.example.demo;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.sql.*;
+import java.util.Objects;
 
 public class Login {
 
@@ -20,16 +26,6 @@ public class Login {
 
     @FXML
     private TextField tfUser;
-
-    @FXML
-    void userLoginAction(ActionEvent event) {
-
-    }
-
-    @FXML
-    void userRegisterAction(ActionEvent event) {
-
-    }
 
     public boolean userLogin(String username, String password) {
 
@@ -47,5 +43,29 @@ public class Login {
             System.out.println("Login Error");}
         return false;
     }
+
+    @FXML
+    void userLoginAction(ActionEvent event) throws IOException {
+
+        if(userLogin(tfUser.getText(), tfPass.getText())) {
+            Stage stage = (Stage) tfUser.getScene().getWindow();
+            Parent root;
+            if(tfUser.getText().equals("admin")) { //omitted check for pass to save complexity
+                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("src/main/resources/org/example/demo/admin.fxml")));
+            } else{
+                root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("src/main/resources/org/example/demo/user.fxml")));
+            }
+            stage.setScene(new Scene(root,600,400));
+        } else {
+            System.out.println("Login failed");
+        }
+    }
+
+    @FXML
+    void userRegisterAction(ActionEvent event) {
+
+    }
+
+
 
 }
