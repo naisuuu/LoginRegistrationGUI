13c40f7a882deb3728b800b441f34e09d3fec2ce
Cyril Mosawan <cjmosawan@yahoo.ie>
2024-07-24 02:58:58 +0100

    configured secure db

--- src/main/java/org/example/demo/DB/DB.java
+++ src/main/java/org/example/demo/DB/DB.java
@@ -9,17 +9,31 @@ import java.sql.Statement;
 public class DB {
     public void setupDB() throws SQLException {
             Connection conn = null;
-
+try{
             String url = "jdbc:sqlite:users.db";
 
-            String createUsersTable = "CREATE TABLE IF NOT EXISTS users (" +
+            String createUsersTableString = "CREATE TABLE IF NOT EXISTS users (" +
                     "user_id int PRIMARY KEY, " +
                     "username varchar(25) NOT NULL, " +
                     "password varchar(25) NOT NULL)";
 
             conn = DriverManager.getConnection(url);
             Statement statement = conn.createStatement();
-            statement.executeUpdate(createUsersTable);
+            statement.executeUpdate(createUsersTableString);
             System.out.println("Users table created");
+
+    } catch (SQLException e) {
+    System.out.println(e.getMessage());
+    throw new RuntimeException(e);
+} finally {
+    try{
+        if (conn != null){
+            conn.close();
+        }
+    } catch (SQLException e) {
+        System.out.println(e.getMessage());
+    }
+}
+
     }
 }
